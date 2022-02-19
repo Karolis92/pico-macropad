f0729fe4525c1f6520b0030465b63433b8c8c976
Karolis92 <Karolis92@users.noreply.github.com>
2022-02-19 19:05:25 +0200

    long press support

--- main.py
+++ main.py
@@ -2,38 +2,41 @@ import board
 import keypad
 from adafruit_hid.consumer_control_code import ConsumerControlCode
 from adafruit_hid.keycode import Keycode
-
 from pico_macropad.encoder import Encoder
-from pico_macropad.hid import CcKey, KbKeys, KbKeys as Key
+from pico_macropad.hid import Key, ConsumerCtrl, Multifunctional
 
 
 def main():
+    # Multifunctional.long_press_duration = 1 # change long press duration
     # create keys to send commands to the computer
-    keys: list[Key] = [
+    keys: list[Key | Multifunctional] = [
         # row 1
-        CcKey(ConsumerControlCode.SCAN_PREVIOUS_TRACK),
-        CcKey(ConsumerControlCode.SCAN_NEXT_TRACK),
-        CcKey(ConsumerControlCode.PLAY_PAUSE),
-        None,  # no key here
+        Multifunctional(short=ConsumerCtrl(ConsumerControlCode.SCAN_PREVIOUS_TRACK),
+                        long=Key(Keycode.A)),
+        ConsumerCtrl(ConsumerControlCode.SCAN_NEXT_TRACK),
+        Multifunctional(short=ConsumerCtrl(ConsumerControlCode.PLAY_PAUSE),
+                        long=Key(Keycode.CONTROL, Keycode.ALT, Keycode.S)),
+        None,
         # row 2
-        KbKeys(Keycode.F17),
-        KbKeys(Keycode.CONTROL, Keycode.ALT, Keycode.A),
-        KbKeys(Keycode.F19),
-        KbKeys(Keycode.F20),
+        Multifunctional(short=Key(Keycode.CONTROL, Keycode.ALT, Keycode.F1),
+                        long=Key(Keycode.CONTROL, Keycode.ALT, Keycode.F2)),
+        Key(Keycode.CONTROL, Keycode.ALT, Keycode.A),
+        Key(Keycode.CONTROL, Keycode.D),
+        Key(Keycode.WINDOWS, Keycode.E),
         # row 3
-        KbKeys(Keycode.F21),
-        KbKeys(Keycode.F22),
-        KbKeys(Keycode.F23),
-        KbKeys(Keycode.F24),
+        Key(Keycode.CONTROL, Keycode.ALT, Keycode.V),
+        Key(Keycode.CONTROL, Keycode.ALT, Keycode.T),
+        Key(Keycode.F12),
+        Key(Keycode.WINDOWS, Keycode.D),
     ]
-    encoder_cw_key = CcKey(ConsumerControlCode.VOLUME_INCREMENT)
-    encoder_ccw_key = CcKey(ConsumerControlCode.VOLUME_DECREMENT)
-    encoder_btn_key = CcKey(ConsumerControlCode.MUTE)
+    encoder_cw_key = ConsumerCtrl(ConsumerControlCode.VOLUME_INCREMENT)
+    encoder_ccw_key = ConsumerCtrl(ConsumerControlCode.VOLUME_DECREMENT)
+    encoder_btn_key = ConsumerCtrl(ConsumerControlCode.MUTE)
 
     # create KeyMatrix to read key presses
     matrix = keypad.KeyMatrix(
-        row_pins=(board.GP18, board.GP17, board.GP16), column_pins=(
-            board.GP19, board.GP20, board.GP21, board.GP22)
+        row_pins=(board.GP18, board.GP17, board.GP16),
+        column_pins=(board.GP19, board.GP20, board.GP21, board.GP22)
     )
 
     # create Encoder to read values
@@ -44,16 +47,16 @@ def main():
         if event:
             key = keys[event.key_number]
             if event.pressed:
-                key.press()
+                key.pressed()
             else:
-                key.release()
+                key.released()
 
         event = encoder.getButtonEvent()
         if event:
             if event.pressed:
-                encoder_btn_key.press()
+                encoder_btn_key.pressed()
             else:
-                encoder_btn_key.release()
+                encoder_btn_key.released()
 
         event = encoder.getPositionEvent()
         if event:
--- pico_macropad/hid.py
+++ pico_macropad/hid.py
@@ -1,33 +1,48 @@
+import time
 from adafruit_hid.consumer_control import ConsumerControl
 from adafruit_hid.keyboard import Keyboard
 import usb_hid
 
 
 class Key:
-    _device: Keyboard | ConsumerControl
-
-    def __init__(self, *keys: str):
-        self._keys = keys
-
-    def press(self):
-        self._device.press(*self._keys)
-
-    def send(self):
-        self._device.send(*self._keys)
-
-    def release(self):
-        self._device.release(*self._keys)
-
-
-class KbKeys(Key):
     _device = Keyboard(usb_hid.devices)
 
+    def __init__(self, *keys: str):
+        self.keys = keys
 
-class CcKey(Key):
+    def send(self):
+        self._device.send(*self.keys)
+
+    def pressed(self):
+        self._device.press(*self.keys)
+
+    def released(self):
+        self._device.release(*self.keys)
+
+
+class ConsumerCtrl(Key):
     _device = ConsumerControl(usb_hid.devices)
 
-    def __init__(self, keys: str):
-        super().__init__(keys)
+    def __init__(self, key: str):
+        super().__init__(key)
 
-    def release(self):
+    def released(self):
         self._device.release()
+
+
+class Multifunctional:
+    long_press_duration = 0.5
+
+    def __init__(self, short: Key, long: Key = None):
+        self.tap = short
+        self.long = long
+
+    def pressed(self):
+        self._pressed = time.monotonic()
+
+    def released(self):
+        released = time.monotonic()
+        if (self.long and self.long_press_duration < released - self._pressed):
+            self.long.send()
+        else:
+            self.tap.send()
